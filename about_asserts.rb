68236e0445347e98f6c427042887963617deeff2
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-10-18 17:20:24 -0700

    Work on koans and add watchr gem

--- about_asserts.rb
+++ about_asserts.rb
@@ -21,7 +21,8 @@ class AboutAsserts < Neo::Koan
   # To understand reality, we must compare our expectations against
   # reality.
   def test_assert_equality
-    expected_value = __
+    # expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert expected_value == actual_value
@@ -29,7 +30,8 @@ class AboutAsserts < Neo::Koan
 
   # Some ways of asserting equality are better than others.
   def test_a_better_way_of_asserting_equality
-    expected_value = __
+    # expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert_equal expected_value, actual_value
